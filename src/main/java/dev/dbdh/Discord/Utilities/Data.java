9cd692fd2f60d249db46ed742868b3223e0f1e4b
ExZiByte <exzibyte@gmail.com>
2019-10-14 21:13:11 -0500

    Added random number generator for JoinData

--- src/main/java/dev/dbdh/Discord/Utilities/Data.java
+++ src/main/java/dev/dbdh/Discord/Utilities/Data.java
@@ -1,9 +1,23 @@
 package dev.dbdh.Discord.Utilities;
 
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberLeaveEvent;
+
+import java.util.Random;
+
 public class Data {
 
     public String getPrefix(){
         return "~";
     }
-    
+
+    public int getRandomNumber(){
+
+        Random RNJesus = new Random();
+        int i = RNJesus.nextInt(23);
+
+        return i;
+    }
+
+
 }
