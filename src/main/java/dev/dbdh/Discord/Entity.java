3780977da7883e7031818b065253f960fa7db8e8
ExZiByte <exzibyte@gmail.com>
2019-10-14 20:33:40 -0500

    Added JDABuilder and Ready event

--- src/main/java/dev/dbdh/Discord/Entity.java
+++ src/main/java/dev/dbdh/Discord/Entity.java
@@ -1,4 +1,28 @@
 package dev.dbdh.Discord;
 
+import dev.dbdh.Discord.Listeners.Ready;
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.exceptions.RateLimitedException;
+
+import javax.security.auth.login.LoginException;
+import java.net.UnknownHostException;
+
 public class Entity {
+
+    public static void main(String[] args) throws LoginException, RateLimitedException, InterruptedException, UnknownHostException {
+        JDABuilder entity = new JDABuilder(AccountType.BOT).setToken(System.getenv("ENTITYDEVTOKEN"));
+
+        entity.setStatus(OnlineStatus.DO_NOT_DISTURB);
+        entity.setActivity(Activity.watching("Loading Bars"));
+
+        entity.addEventListeners(
+                new Ready()
+        );
+
+        entity.build();
+    }
+
 }
