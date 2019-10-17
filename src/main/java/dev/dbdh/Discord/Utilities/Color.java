7468666abb5869756bf2ab14b6ef55b28cd5d1c2
ExZiByte <exzibyte@gmail.com>
2019-10-16 19:02:58 -0500

    Unmethodized color grabber

--- src/main/java/dev/dbdh/Discord/Utilities/Color.java
+++ src/main/java/dev/dbdh/Discord/Utilities/Color.java
@@ -1,10 +1,10 @@
 package dev.dbdh.Discord.Utilities;
 
 public class Color {
-    public int deepRed(){ return 0xb20c0c; }
-    public int red() {return 0xCC0000; }
-    public int yellow() {return 0xFFFF00; }
-    public int gold() { return 0xCCAC00; }
-    public int darkGreen() {return 0x228B22; }
-    public int babyBlue(){ return 0xa1e5f3; }
+    public int deepRed = 0xb20c0c;
+    public int red = 0xCC0000;
+    public int yellow = 0xFFFF00;
+    public int gold = 0xCCAC00;
+    public int darkGreen = 0x228B22;
+    public int babyBlue = 0xa1e5f3;
 }
