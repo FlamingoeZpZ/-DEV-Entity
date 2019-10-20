9c00ee6416c5b3dfefd6ae70bb5de1e56dfc5811
ExZiByte <exzibyte@gmail.com>
2019-10-20 16:52:00 -0500

    Hopefully fixed database build command:

--- src/main/java/dev/dbdh/Discord/Listeners/Economy/AddEveryoneToDatabase.java
+++ src/main/java/dev/dbdh/Discord/Listeners/Economy/AddEveryoneToDatabase.java
@@ -12,6 +12,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.bson.Document;
 
 import java.time.Instant;
+import java.util.concurrent.TimeUnit;
 
 import static com.mongodb.client.model.Filters.eq;
 
@@ -26,18 +27,20 @@ public class AddEveryoneToDatabase extends ListenerAdapter {
             db.connect();
             MongoCollection<Document> members = db.getCollection("members");
             for(Member member: event.getGuild().getMembers()) {
-                if (members.find(eq("memberId", member.getUser().getId())).first() == null) {
-                    Document perksActive = new Document(new BasicDBObject("aceInTheHole", 0).append("pharmacy", 0).append("plunderers", 0));
-                    Document memberInfo = new Document(new BasicDBObject("memberId", event.getMember().getUser().getId()).append("memberName", event.getMember().getUser().getName() + "#" + event.getMember().getUser().getDiscriminator()).append("balance", 0).append("perksActive", perksActive).append("eventWins", 0));
-                    members.insertOne(memberInfo);
-                    eb.setDescription("Added " + member.getUser().getName() + " to Dead by Daylight Hub Database, Thank you for letting us harvest your data!");
-                    eb.setColor(color.getRandomColor());
-                    eb.setTimestamp(Instant.now());
-                    eb.setFooter("Entity Data Harvester", member.getUser().getEffectiveAvatarUrl());
+                if (members.find(eq("memberID", member.getUser().getId())).first() == null) {
+                    if (!member.getUser().isBot()) {
+                        Document perksActive = new Document(new BasicDBObject("aceInTheHole", 0).append("pharmacy", 0).append("plunderers", 0));
+                        Document memberInfo = new Document(new BasicDBObject("memberId", event.getMember().getUser().getId()).append("memberName", event.getMember().getUser().getName() + "#" + event.getMember().getUser().getDiscriminator()).append("balance", 0).append("perksActive", perksActive).append("eventWins", 0));
+                        members.insertOne(memberInfo);
+                        eb.setDescription("Added " + member.getUser().getName() + " to Dead by Daylight Hub Database, Thank you for letting us harvest your data!");
+                        eb.setColor(color.getRandomColor());
+                        eb.setTimestamp(Instant.now());
+                        eb.setFooter("Entity Data Harvester", member.getUser().getEffectiveAvatarUrl());
 
-                    event.getChannel().sendMessage(eb.build()).queue((message) ->{
-                        eb.clear();
-                    });
+                        event.getChannel().sendMessage(eb.build()).queueAfter(5, TimeUnit.SECONDS, ((message) -> {
+                            eb.clear();
+                        }));
+                    }
                 }
             }
             db.close();
