a95dcb043bd4e80978d9fde0d1f355f8f35d6c6e
ExZiByte <exzibyte@gmail.com>
2019-10-20 14:56:47 -0500

    RAINBOW HARVEST

--- src/main/java/dev/dbdh/Discord/Listeners/Economy/AddEveryoneToDatabase.java
+++ src/main/java/dev/dbdh/Discord/Listeners/Economy/AddEveryoneToDatabase.java
@@ -2,6 +2,7 @@ package dev.dbdh.Discord.Listeners.Economy;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoCollection;
+import dev.dbdh.Discord.Utilities.Color;
 import dev.dbdh.Discord.Utilities.Data;
 import dev.dbdh.Discord.Utilities.Database;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -17,6 +18,7 @@ import static com.mongodb.client.model.Filters.eq;
 public class AddEveryoneToDatabase extends ListenerAdapter {
     public void onGuildMessageReceived(GuildMessageReceivedEvent event){
         String[] args = event.getMessage().getContentRaw().split("\\s+");
+        Color color = new Color();
         Data data = new Data();
         Database db = new Database();
         EmbedBuilder eb = new EmbedBuilder();
@@ -28,8 +30,10 @@ public class AddEveryoneToDatabase extends ListenerAdapter {
                     Document perksActive = new Document(new BasicDBObject("aceInTheHole", 0).append("pharmacy", 0).append("plunderers", 0));
                     Document memberInfo = new Document(new BasicDBObject("memberId", event.getMember().getUser().getId()).append("memberName", event.getMember().getUser().getName() + "#" + event.getMember().getUser().getDiscriminator()).append("balance", 0).append("perksActive", perksActive).append("eventWins", 0));
                     members.insertOne(memberInfo);
-                    eb.setDescription("Added " + member.getUser().getName());
+                    eb.setDescription("Added " + member.getUser().getName() + " to Dead by Daylight Hub Database, Thank you for letting us harvest your data!");
+                    eb.setColor(color.getRandomColor());
                     eb.setTimestamp(Instant.now());
+                    eb.setFooter("Entity Data Harvester", member.getUser().getEffectiveAvatarUrl());
 
                     event.getChannel().sendMessage(eb.build()).queue((message) ->{
                         eb.clear();
