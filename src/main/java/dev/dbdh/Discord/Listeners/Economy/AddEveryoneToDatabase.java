c32da4d9bfbdd3b6fc8eebdda04ccbc6da7d83fd
ExZiByte <exzibyte@gmail.com>
2019-10-20 14:50:39 -0500

    Made a add everyone to database command

--- src/main/java/dev/dbdh/Discord/Listeners/Economy/AddEveryoneToDatabase.java
+++ src/main/java/dev/dbdh/Discord/Listeners/Economy/AddEveryoneToDatabase.java
@@ -4,10 +4,13 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoCollection;
 import dev.dbdh.Discord.Utilities.Data;
 import dev.dbdh.Discord.Utilities.Database;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.bson.Document;
+
+import java.time.Instant;
 
 import static com.mongodb.client.model.Filters.eq;
 
@@ -16,6 +19,7 @@ public class AddEveryoneToDatabase extends ListenerAdapter {
         String[] args = event.getMessage().getContentRaw().split("\\s+");
         Data data = new Data();
         Database db = new Database();
+        EmbedBuilder eb = new EmbedBuilder();
         if(args[0].equalsIgnoreCase("addEveryoneStoopidBot")){
             db.connect();
             MongoCollection<Document> members = db.getCollection("members");
@@ -24,6 +28,12 @@ public class AddEveryoneToDatabase extends ListenerAdapter {
                     Document perksActive = new Document(new BasicDBObject("aceInTheHole", 0).append("pharmacy", 0).append("plunderers", 0));
                     Document memberInfo = new Document(new BasicDBObject("memberId", event.getMember().getUser().getId()).append("memberName", event.getMember().getUser().getName() + "#" + event.getMember().getUser().getDiscriminator()).append("balance", 0).append("perksActive", perksActive).append("eventWins", 0));
                     members.insertOne(memberInfo);
+                    eb.setDescription("Added " + member.getUser().getName());
+                    eb.setTimestamp(Instant.now());
+
+                    event.getChannel().sendMessage(eb.build()).queue((message) ->{
+                        eb.clear();
+                    });
                 }
             }
             db.close();
