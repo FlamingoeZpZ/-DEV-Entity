c49e2a836a87af85843d5140518c41fb686ea8a7
ExZiByte <exzibyte@gmail.com>
2019-10-20 21:42:32 -0500

    I fucked up

--- src/main/java/dev/dbdh/Discord/Listeners/Economy/EditCoins.java
+++ src/main/java/dev/dbdh/Discord/Listeners/Economy/EditCoins.java
@@ -42,7 +42,7 @@ public class EditCoins extends ListenerAdapter {
                     if (Arrays.stream(removeAliases).anyMatch(args[1]::equals)) {
                         String name = Arrays.stream(args).skip(3).collect(Collectors.joining(" "));
                         Member mentioned = event.getGuild().getMembersByName(name, true).get(0);
-                        eb.setDescription("Successfully removed " + args[1] + " coins from " + mentioned.getAsMention() + "'s balance.");
+                        eb.setDescription("Successfully removed " + args[2] + " coins from " + mentioned.getAsMention() + "'s balance.");
                         eb.setColor(color.getRandomColor());
                         eb.setTimestamp(Instant.now());
                         eb.setFooter("Entity Economy Update", data.getSelfAvatar(event));
@@ -61,7 +61,7 @@ public class EditCoins extends ListenerAdapter {
                     } else if (Arrays.stream(addAliases).anyMatch(args[1]::equals)) {
                         String name = Arrays.stream(args).skip(3).collect(Collectors.joining(" "));
                         Member mentioned = event.getGuild().getMembersByName(name, true).get(0);
-                        eb.setDescription("Successfully added " + args[1] + " coins to " + mentioned.getAsMention() + "'s balance.");
+                        eb.setDescription("Successfully added " + args[2] + " coins to " + mentioned.getAsMention() + "'s balance.");
                         eb.setColor(color.getRandomColor());
                         eb.setTimestamp(Instant.now());
                         eb.setFooter("Entity Economy Update", data.getSelfAvatar(event));
