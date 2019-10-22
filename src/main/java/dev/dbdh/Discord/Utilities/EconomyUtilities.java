85347a7930137c4ffc40e343ea0f2559b2865d72
ExZiByte <exzibyte@gmail.com>
2019-10-21 20:03:38 -0500

    Trying to fix NPE from EconomyUtilities

--- src/main/java/dev/dbdh/Discord/Utilities/EconomyUtilities.java
+++ src/main/java/dev/dbdh/Discord/Utilities/EconomyUtilities.java
@@ -10,13 +10,13 @@ import static com.mongodb.client.model.Filters.eq;
 public class EconomyUtilities {
 
     public void addCoins(GuildMessageReceivedEvent event, String memberName, Integer coins){
-        String memberID = event.getGuild().getMembersByName(memberName, true).get(0).getId().toString();
+        String memberID = event.getGuild().getMembersByName(memberName, true).get(0).getId();
         Database db = new Database();
         db.connect();
         MongoCollection<Document> members = db.getCollection("members");
         Document member = members.find(eq("memberId", memberID)).first();
 
-        Integer balance = member.getInteger("balance");
+        Integer balance = Integer.parseInt(member.get("balance").toString());
 
         Bson newMemberDoc = new Document("balance", balance + coins);
         Bson updateMemberDoc = new Document("$set", newMemberDoc);
@@ -32,7 +32,7 @@ public class EconomyUtilities {
         MongoCollection<Document> members = db.getCollection("members");
         Document member = members.find(eq("memberId", memberID)).first();
 
-        Integer balance = member.getInteger("balance");
+        Integer balance = Integer.parseInt(member.get("balance").toString());
 
         Bson newMemberDoc = new Document("balance", balance - coins);
         Bson updateMemberDoc = new Document("$set", newMemberDoc);
