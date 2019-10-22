aa56b397e4a84254f44b26383095d49b20f7016d
ExZiByte <exzibyte@gmail.com>
2019-10-21 21:40:09 -0500

    Added counter class to count things

--- src/main/java/dev/dbdh/Discord/Utilities/Counter.java
+++ src/main/java/dev/dbdh/Discord/Utilities/Counter.java
@@ -1,4 +1,18 @@
 package dev.dbdh.Discord.Utilities;
 
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
 public class Counter {
+
+    public Integer getMemberCount(GuildMemberJoinEvent event){
+        Integer bots = 0;
+        for (Member member: event.getGuild().getMembers()){
+            if(member.getUser().isBot()){
+                bots += 1;
+            }
+        }
+        return event.getGuild().getMembers().size() - bots;
+    }
 }
