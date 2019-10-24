f13e0242797b82a3881ed8e3ec7c1316748bd4ab
ExZiByte <exzibyte@gmail.com>
2019-10-23 21:40:01 -0500

    Hopefully fixed the gitignore

--- src/main/java/dev/dbdh/Discord/Listeners/Fun/LookingToPlay.java
+++ src/main/java/dev/dbdh/Discord/Listeners/Fun/LookingToPlay.java
@@ -66,6 +66,36 @@ public class LookingToPlay extends ListenerAdapter {
                             event.getGuild().removeRoleFromMember(event.getMember(), event.getGuild().getRoleById("634878800185786378")).queue();
 
                         }
+                    } else {
+                        if (Arrays.stream(xboxAliases).anyMatch(args[1]::equalsIgnoreCase)) {
+                            event.getChannel().sendMessage(event.getMember().getAsMention() + " is looking to play on Xbox " + event.getGuild().getRoleById("606527228074786851").getAsMention()).queue();
+                            event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("606527228074786851")).queue();
+                        } else if (Arrays.stream(ps4Aliases).anyMatch(args[1]::equalsIgnoreCase)) {
+                            event.getChannel().sendMessage(event.getMember().getAsMention() + " is looking to play on Playstation " + event.getGuild().getRoleById("606527283582468106").getAsMention()).queue();
+                            event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("606527283582468106")).queue();
+                        } else if (Arrays.stream(pcAliases).anyMatch(args[1]::equalsIgnoreCase)) {
+                            event.getChannel().sendMessage(event.getMember().getAsMention() + " is looking to play on PC " + event.getGuild().getRoleById("541091501908951041").getAsMention()).queue();
+                            event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("541091501908951041")).queue();
+                        } else if (Arrays.stream(mobileAliases).anyMatch(args[1]::equalsIgnoreCase)) {
+                            event.getChannel().sendMessage(event.getMember().getAsMention() + " is looking to play on Mobile " + event.getGuild().getRoleById("606531352690688035").getAsMention()).queue();
+                            event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("606531352690688035")).queue();
+                        } else if (Arrays.stream(switchAliases).anyMatch(args[1]::equalsIgnoreCase)) {
+                            event.getChannel().sendMessage(event.getMember().getAsMention() + " is looking to play on Nintendo Switch " + event.getGuild().getRoleById("634878800185786378").getAsMention()).queue();
+                            event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("634878800185786378")).queue();
+                        } else if (args[1].equalsIgnoreCase("stop")) {
+                            event.getChannel().sendMessage("Stopped matchmaking").queue();
+                            //XBOX
+                            event.getGuild().removeRoleFromMember(event.getMember(), event.getGuild().getRoleById("606527228074786851")).queue();
+                            //PS4
+                            event.getGuild().removeRoleFromMember(event.getMember(), event.getGuild().getRoleById("606527283582468106")).queue();
+                            //PC
+                            event.getGuild().removeRoleFromMember(event.getMember(), event.getGuild().getRoleById("541091501908951041")).queue();
+                            //Mobile
+                            event.getGuild().removeRoleFromMember(event.getMember(), event.getGuild().getRoleById("606531352690688035")).queue();
+                            //Switch
+                            event.getGuild().removeRoleFromMember(event.getMember(), event.getGuild().getRoleById("634878800185786378")).queue();
+
+                        }
                     }
                 }
             }
