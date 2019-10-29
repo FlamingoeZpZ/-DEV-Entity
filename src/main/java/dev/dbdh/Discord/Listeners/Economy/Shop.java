06e07c2a1b2885466df23f813b4aa74c2b44a48f
ExZiByte <exzibyte@gmail.com>
2019-10-28 20:25:39 -0500

    no errors

--- src/main/java/dev/dbdh/Discord/Listeners/Economy/Shop.java
+++ src/main/java/dev/dbdh/Discord/Listeners/Economy/Shop.java
@@ -11,26 +11,33 @@ import java.util.Arrays;
 public class Shop {
     Color color = new Color(); // Each shop has different colour identifiers
     EmbedBuilder eb = new EmbedBuilder();
+
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
         Aliases Al = new Aliases();
         Data data = new Data();
         String args[] = event.getMessage().getContentRaw().split("\\s+");
         String command = data.getPrefix() + args[0];
-        if (Arrays.stream(Al.shopAliases).anyMatch( command::equalsIgnoreCase)) {
-            
+        if (Arrays.stream(Al.shopAliases).anyMatch(command::equalsIgnoreCase)) {
+            if (args.length == 1) {
+
+            } else {
+                
+            }
+
+        }
     }
 }
-
 /*
-Shop.setDescription("How to use the " + event.getGuild().getName() + " shop"
-                    + data.getPrefix() + "shop <Type> <ID> || <> means required\n **Types:**"
-                    + "\nChest: Contains perks used for increasing luck and prizes in the ~chest command\n"
-                    + "```\nID: 1 = Pharmacy (Increases coins earned)\n"
-                    + "ID: 2 = Ace in the Hole (Increases XP Gained)\n"
-                    + "ID: 3 = Plunder's Instinct (Increases amount of chests opened at a time)\n```"
-                    + "\nChase: Contains perks and boosters used for increasing luck and rewards in the ~chase command\n"
-                    + "``ID: 1 = Quick and Quiet (Allows for extra mistakes)``\n"
-                    + "``ID: 2 = Dead Hard (Increases XP Gain)``\n"
-                    + "``ID: 3 = Decisive Strike(Increases Gold Gain)``"
-                    + "``ID: 3 = Decisive Strike(Increases Gold Gain)``"
-                    */
+ * Shop.setDescription("How to use the " + event.getGuild().getName() + " shop"
+ * + data.getPrefix() + "shop <Type> <ID> || <> means required\n **Types:**" +
+ * "\nChest: Contains perks used for increasing luck and prizes in the ~chest command\n"
+ * + "```\nID: 1 = Pharmacy (Increases coins earned)\n" +
+ * "ID: 2 = Ace in the Hole (Increases XP Gained)\n" +
+ * "ID: 3 = Plunder's Instinct (Increases amount of chests opened at a time)\n```"
+ * +
+ * "\nChase: Contains perks and boosters used for increasing luck and rewards in the ~chase command\n"
+ * + "``ID: 1 = Quick and Quiet (Allows for extra mistakes)``\n" +
+ * "``ID: 2 = Dead Hard (Increases XP Gain)``\n" +
+ * "``ID: 3 = Decisive Strike(Increases Gold Gain)``" +
+ * "``ID: 3 = Decisive Strike(Increases Gold Gain)``"
+ */
