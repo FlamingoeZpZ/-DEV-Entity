438b6eeea49cc1f54c835cdda81c7c934a734414
ExZiByte <exzibyte@gmail.com>
2019-11-03 14:51:59 -0600

    Testing database reading with relation to dates

--- src/main/java/dev/dbdh/Discord/Listeners/Miscellaneous/TestDate.java
+++ src/main/java/dev/dbdh/Discord/Listeners/Miscellaneous/TestDate.java
@@ -1,8 +1,11 @@
 package dev.dbdh.Discord.Listeners.Miscellaneous;
 
+import com.mongodb.client.MongoCollection;
 import dev.dbdh.Discord.Utilities.Database;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.bson.Document;
+import org.bson.conversions.Bson;
 
 import static com.mongodb.client.model.Filters.eq;
 
@@ -14,8 +17,11 @@ public class TestDate extends ListenerAdapter {
         if(args[0].equalsIgnoreCase("printshit")){
             db.connect();
             System.out.println(System.currentTimeMillis());
-            System.out.println(db.getCollection("members").find(eq("memberId", event.getMember().getId())).first().get("cooldowns.daily"));
-            //System.out.println(db.getCollection("members").find(eq("memberId", event.getMember().getId())).first().get("cooldowns.daily") + 86400000);
+            MongoCollection<Document> members = db.getCollection("members");
+            Document member = members.find(eq("memberId", event.getMember().getId())).first();
+            Document cooldowns = (Document) member.get("cooldowns");
+            long daily = cooldowns.getLong("daily");
+            System.out.println(daily);
             db.close();
         }
     }
