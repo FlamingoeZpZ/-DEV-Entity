a9782bc998fae2a200a7f38889b8d316499ba533
ExZiByte <exzibyte@gmail.com>
2019-11-04 19:02:01 -0600

    Fixed retry later date on daily cooldown not ready embed

--- src/main/java/dev/dbdh/Discord/Listeners/Economy/Daily.java
+++ src/main/java/dev/dbdh/Discord/Listeners/Economy/Daily.java
@@ -37,7 +37,7 @@ public class Daily extends ListenerAdapter {
                     eb.clear();
                 });
             } else {
-                SimpleDateFormat formatter = new SimpleDateFormat("HH:mm MM-dd-yyyy");
+                SimpleDateFormat formatter = new SimpleDateFormat("HH:mm MM-dd-YYYY");
                 String formattedDate = formatter.format(Date.from(Instant.ofEpochSecond(ecu.getCooldown(event, event.getMember().getUser().getId(), "daily"))));
                 eb.setDescription("Your daily cooldown is not ready.\nTry again at " + formattedDate);
                 eb.setColor(color.getRandomColor());
