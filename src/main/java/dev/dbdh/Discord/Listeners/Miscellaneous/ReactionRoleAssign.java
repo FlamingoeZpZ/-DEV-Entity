3b993f052066f7bd5ee880de596bbe0f92add793
ExZiByte <exzibyte@gmail.com>
2019-12-11 23:05:13 -0600

    Switched from switch/case to if statememts cause switch/case is broke

--- src/main/java/dev/dbdh/Discord/Listeners/Miscellaneous/ReactionRoleAssign.java
+++ src/main/java/dev/dbdh/Discord/Listeners/Miscellaneous/ReactionRoleAssign.java
@@ -12,29 +12,39 @@ public class ReactionRoleAssign extends ListenerAdapter {
                 Database db = new Database();
                 db.connect();
                 if(event.getMessageId().equals(db.getCollection("guild").find().first().getString("roleAssignMessageID"))){
-                    switch(event.getReactionEmote().getId()){
+                    if(event.getReactionEmote().getId().equals("575437440450560000")){
                         //Killer
-                        case "575437440450560000": event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("540744380416262185")).queue();
-                            //Survivor
-                        case "579132892370829333": event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("540744386506653712")).queue();
-                            //All the time
-                        case "649444100982177792": event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("540755651731783680")).queue();
-                            //Occasionally
-                        case "649444100864475138": event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("540755676541222912")).queue();
-                            //Rarely
-                        case "649444100952817686": event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("540755698255003699")).queue();
-                            //NSFW
-                        case "573230699885232140": event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("543652286279319552")).queue();
-                            //Game
-                        case "573235478711500837": event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("575326203901116474")).queue();
-                            //Streamer Ann
-                        case "583392850067324928": event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("540759805481189379")).queue();
-                            //Server Ann
-                        case "575437537074479114": event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("589450072488542219")).queue();
-                            //Game Ann
-                        case "577557877343125518": event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("589450041542836243")).queue();
-                            //Event Ann
-                        case "575437440509280296": event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("629135819826921493")).queue();
+                        event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("540744380416262185")).queue();
+                    } else if(event.getReactionEmote().getId().equals("579132892370829333")){
+                        //Survivor
+                        event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("540744386506653712")).queue();
+                    } else if(event.getReactionEmote().getId().equals("649444100982177792")){
+                        //All the time
+                        event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("540755651731783680")).queue();
+                    } else if(event.getReactionEmote().getId().equals("649444100864475138")){
+                        //Occasionally
+                        event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("540755676541222912")).queue();
+                    } else if(event.getReactionEmote().getId().equals("649444100952817686")){
+                        //Rarely
+                        event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("540755698255003699")).queue();
+                    } else if(event.getReactionEmote().getId().equals("573230699885232140")){
+                        //NSFW
+                        event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("543652286279319552")).queue();
+                    } else if(event.getReactionEmote().getId().equals("573235478711500837")){
+                        //Game
+                        event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("575326203901116474")).queue();
+                    } else if (event.getReactionEmote().getId().equals("583392850067324928")) {
+                        //Streamer Announcements
+                        event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("540759805481189379")).queue();
+                    } else if (event.getReactionEmote().getId().equals("575437537074479114")) {
+                        //Server Announcements
+                        event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("589450072488542219")).queue();
+                    } else if (event.getReactionEmote().getId().equals("577557877343125518")) {
+                        //Game Announcements
+                        event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("589450041542836243")).queue();
+                    } else if (event.getReactionEmote().getId().equals("575437440509280296")) {
+                        //Event Announcements
+                        event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("629135819826921493")).queue();
                     }
                 }
             }
