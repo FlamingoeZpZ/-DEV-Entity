83dca4767fd15837d7bf434152a70ccde8958557
Flamingo L <gabekotton@gmail.com>
2020-01-07 22:30:40 -0500

    smol update

--- src/main/java/dev/dbdh/Discord/Listeners/Fun/Custom/AddToRole.java
+++ src/main/java/dev/dbdh/Discord/Listeners/Fun/Custom/AddToRole.java
@@ -17,7 +17,7 @@ public class AddToRole extends ListenerAdapter {
         if(args[0].equalsIgnoreCase(Data.getPrefix() + "storm" )){
             event.getGuild().removeRoleFromMember(event.getMember(), event.getGuild().getRoleById("664300092831825942")).queue();
             event.getMessage().delete().queue();
-            eb.setDescription("Thank you for watching " + event.getGuild().getMemberById("664308445213753366").getEffectiveName() + " streams and supporting the " + event.getGuild().getName() +"!");
+            eb.setDescription("Thank you for watching StormGriffon streams and supporting the " + event.getGuild().getName() +"!");
             eb.setColor(color.successGreen);
             eb.setTimestamp(Instant.now());
             eb.setFooter("Entity Role Added", data.getSelfAvatar(event));
