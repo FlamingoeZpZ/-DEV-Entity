143197867b07c9ebd31d2dba7bb176cc13be355a
Flamingo L <gabekotton@gmail.com>
2020-02-08 13:19:55 -0500

    Too lazy to write these

--- src/main/java/dev/dbdh/Discord/Utilities/EconomyUtilities.java
+++ src/main/java/dev/dbdh/Discord/Utilities/EconomyUtilities.java
@@ -105,7 +105,7 @@ public class EconomyUtilities {
         Database.connect();
         MongoCollection<Document> members = Database.getCollection("members");
         Document member = members.find(eq("memberId", memberID)).first();
-        int XPChange = Integer.parseInt(member.get("xp").toString()) + xp;
+        int XPChange = (member.getInteger("freeBasicCooldown") + xp);
         int currentLevel = getLevel(event, memberID);
         int levelCost;
         while (true) {
